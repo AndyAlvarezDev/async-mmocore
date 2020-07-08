e57d0b2a7f8bf22171c9f371f3f1b9c6fd53fe7e
Andrés Alvarez <learnware.tech@gmail.com>
2020-07-07 23:27:41 -0300

    Mmocore Async

---
+++ src/main/io.github.andyalvarezdev.mmocore/io/github/andyalvarezdev/mmocore/Connection.java
@@ -0,0 +1,114 @@
+package io.github.andyalvarezdev.mmocore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+
+import static java.util.Objects.nonNull;
+
+public class Connection<T extends Client<Connection<T>>> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Connection.class);
+
+    private final AsynchronousSocketChannel channel;
+    private final ReadHandler<T> readHandler;
+    private final WriteHandler<T> writeHandler;
+    private T client;
+
+    private ByteBuffer readingBuffer;
+    private ByteBuffer writingBuffer;
+
+    Connection(AsynchronousSocketChannel channel, ReadHandler<T> readHandler, WriteHandler<T> writeHandler) {
+        this.channel = channel;
+        this.readHandler = readHandler;
+        this.writeHandler = writeHandler;
+    }
+
+    void setClient(T client) {
+        this.client = client;
+    }
+
+    final void read() {
+        if(channel.isOpen()) {
+            channel.read(readingBuffer, client, readHandler);
+        }
+    }
+
+    final void readHeader() {
+        if(channel.isOpen()) {
+            releaseReadingBuffer();
+            readingBuffer = client.getResourcePool().getHeaderBuffer();
+            read();
+        }
+    }
+
+    void read(int size) {
+        if(channel.isOpen()) {
+            readingBuffer = client.getResourcePool().recycleAndGetNew(readingBuffer, size);
+            read();
+        }
+    }
+
+    final void write(byte[] data, int size) {
+        if(!channel.isOpen()) {
+            return;
+        }
+        writingBuffer = client.getResourcePool().recycleAndGetNew(writingBuffer, size);
+        writingBuffer.put(data, 0, size);
+        writingBuffer.flip();
+        write();
+    }
+
+    final void write() {
+        if(channel.isOpen() && nonNull(writingBuffer)) {
+            channel.write(writingBuffer, client, writeHandler);
+        }
+    }
+
+    ByteBuffer getReadingBuffer() {
+        return readingBuffer;
+    }
+
+    private void releaseReadingBuffer() {
+        client.getResourcePool().recycleBuffer(readingBuffer);
+        readingBuffer=null;
+    }
+
+    void releaseWritingBuffer() {
+        client.getResourcePool().recycleBuffer(writingBuffer);
+        writingBuffer = null;
+    }
+
+    void close() {
+        releaseReadingBuffer();
+        releaseWritingBuffer();
+        try {
+            channel.shutdownInput();
+            channel.shutdownOutput();
+            channel.close();
+        } catch (IOException e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
+    }
+
+    String getRemoteAddress() {
+        try {
+            InetSocketAddress address = (InetSocketAddress) channel.getRemoteAddress();
+            return address.getAddress().getHostAddress();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    boolean isOpen() {
+        try {
+            return channel.isOpen() && nonNull(channel.getRemoteAddress());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/io.github.andyalvarezdev.mmocore/io/github/andyalvarezdev/mmocore/ReadHandler.java
@@ -0,0 +1,98 @@
+package io.github.andyalvarezdev.mmocore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.CompletionHandler;
+
+import static io.github.andyalvarezdev.mmocore.Client.HEADER_SIZE;
+import static java.util.Objects.nonNull;
+
+class ReadHandler<T extends Client<Connection<T>>> implements CompletionHandler<Integer, T> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReadHandler.class);
+
+    private final PacketHandler<T> packetHandler;
+    private final PacketExecutor<T> executor;
+
+    ReadHandler(PacketHandler<T> packetHandler, PacketExecutor<T> executor) {
+        this.packetHandler = packetHandler;
+        this.executor =  executor;
+    }
+
+    @Override
+    public void completed(Integer bytesRead, T client) {
+        if(!client.isConnected()) {
+            return;
+        }
+
+        LOGGER.debug("Reading {} from {}", bytesRead, client);
+        if(bytesRead < 0 ) {
+            client.disconnect();
+            return;
+        }
+        
+        if(bytesRead < client.getExpectedReadSize()) {
+            client.resumeRead(bytesRead);
+            return;
+        }
+
+        if(client.isReadingPayload()) {
+            handlePayload(client);
+        } else {
+            handleHeader(client);
+        }
+    }
+
+    private void handleHeader(T client) {
+        ByteBuffer buffer = client.getConnection().getReadingBuffer();
+        buffer.flip();
+        int dataSize = Short.toUnsignedInt(buffer.getShort()) - HEADER_SIZE;
+        client.readPayload(dataSize);
+    }
+
+    private void handlePayload(T client) {
+        ByteBuffer buffer = client.getConnection().getReadingBuffer();
+        buffer.flip();
+        parseAndExecutePacket(client, buffer);
+        client.read();
+    }
+
+    private void parseAndExecutePacket(T client, ByteBuffer incomingBuffer) {
+        LOGGER.debug("Trying to parse data");
+        int dataSize = incomingBuffer.remaining();
+        byte[] data = new byte[dataSize];
+        incomingBuffer.get(data);
+
+        boolean decrypted = client.decrypt(data, 0, dataSize);
+
+        if(decrypted) {
+            PacketBuffer buffer = PacketBuffer.of(data, 0);
+            ReadablePacket<T> packet = packetHandler.handlePacket(buffer, client);
+            LOGGER.debug("Data parsed to packet {}", packet);
+            if(nonNull(packet)) {
+                packet.init(client, buffer);
+                execute(packet);
+            }
+        }
+    }
+
+    private void execute(ReadablePacket<T> packet) {
+        if(packet.read()) {
+            LOGGER.debug("packet {} was read from client {}", packet, packet.client);
+            executor.execute(packet);
+        }
+     }
+
+    @Override
+    public void failed(Throwable e, T client) {
+        if(client.isConnected()) {
+            client.disconnect();
+        }
+        if( !(e instanceof ClosedChannelException)) {
+            LOGGER.warn("Failed to read from {}", client, e);
+        }
+    }
+}
